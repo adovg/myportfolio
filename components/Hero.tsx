9fbb6a2d6d1ba8d1212ab623354a8ce474ebd9d5
adovg <kroatoan@i.ua>
2025-08-17 22:19:50 +0300

    add content to about section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,15 +3,30 @@ import React from "react";
 import { useContext } from "react";
 import { LanguageContext } from "@/context/LocaleContext";
 import { translations } from "@/lib/translations";
+import styles from "./Hero.module.scss";
 
 const Hero = () => {
   const { lang } = useContext(LanguageContext)!;
   return (
-    <div className="hero">
-      <h1>{translations[lang].welcome}</h1>
-      <p>{translations[lang].about}</p>
-    </div>
+    <section className={styles.hero}>
+      <h1>
+        <span>Hello, I am </span>
+        <span className={styles.title__gradient}>Frontend Developer</span>
+      </h1>
+      <h2>
+        <span>
+          I create modern web applications with a focus on user experience
+        </span>
+        <span>
+          I specialize in React, Next.js, TypeScript, and modern web
+          technologies
+        </span>
+      </h2>
+    </section>
   );
 };
 
 export default Hero;
+
+// <h1>{translations[lang].welcome}</h1>
+// <p>{translations[lang].about}</p>
