49292cd924d3431030f01caac45e52e032ac34bf
adovg <kroatoan@i.ua>
2025-08-29 19:05:04 +0300

    add modal window

--- components/PortfolioItem.jsx
+++ components/PortfolioItem.jsx
@@ -1,21 +1,25 @@
+'use client'
 import React from 'react'
-import {mockData} from '@/data/mock'
+// import {mockData} from '@/data/mock'
+import { useState } from 'react'
 import styles from  './PortfolioItem.module.scss'
 
+
 const PortfolioItem = ({projects, title}) => {
-  console.log(projects)
+  const [selected, setSelected] = useState(null)
+  // console.log(projects)
   return (
     <>
-       <h2 className={styles.portfolio__title}>{title}</h2>
+    <h2 className={styles.portfolio__title}>{title}</h2>
     <div className={styles.portfolio__wrapper}>
  
     {projects.map((item) => (
-      <article key={item.id} className={styles.portfolio__item}>
-        <div className="portfolio-item__img">
+      <article key={item.id} className={styles["portfolio-item"]} onClick={() => setSelected(item)} >
+        <div>
             <img src={item.image} alt="" />
         </div>
-        <div className="portfolion-item__content">
-            <h3>{item.title}</h3>
+        <div className={styles["portfolio-item__info"]}>
+            <h3 className={styles["portfolio-item__title"]}>{item.title}</h3>
             <p>{item.description}</p>
             <p>{item.longDescription}</p>
             {item.technologies.map((tech, index) => (
@@ -27,6 +31,25 @@ const PortfolioItem = ({projects, title}) => {
     ))}
 
     </div>
+
+     {selected && (
+        <div className={styles.modalOverlay} onClick={() => setSelected(null)}>
+          <div
+            className={styles.modalContent}
+            onClick={e => e.stopPropagation()}
+          >
+            <button onClick={() => setSelected(null)}>Закрыть</button>
+            <img src={selected.image} alt="" />
+            <h3>{selected.title}</h3>
+            <p>{selected.description}</p>
+            <p>{selected.longDescription}</p>
+            {selected.technologies.map((tech, index) => (
+              <span key={index}>{tech}</span>
+            ))}
+          </div>
+        </div>
+      )}
+
       </>
   )
 
